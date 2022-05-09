fe72b24598c6e994ebcbeb966d1ba4c9051ab5da
vlad3216 <103311342+vlad3216@users.noreply.github.com>
2022-05-10 01:21:28 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,17 +1,14 @@
+import random
 import time
 hello = print('Сейчас вылезет змеяяя!!!')
 body = '*'
-time.sleep(1)
+head = '0_0'
 try:
   while True:
-      time.sleep(0.1)
-      print('{:>6}'.format(body))
-      time.sleep(0.1)
-      print('{:>5}'.format(body))
-      time.sleep(0.1)
-      print('{:>5}'.format(body))
-      time.sleep(0.1)
-      print('{:>6}'.format(body))
+      time.sleep(0.3)
+      body_pos = '{:>4}'.format(body) , '{:>5}'.format(body), '{:>6}'.format(body)
+      head_pos = '{:>6}'.format(head) , '{:>5}'.format(head), '{:>5}'.format(head)
+      print(random.choice(body_pos))
 except KeyboardInterrupt:
-      print('0_0')
+      print(random.choice(head_pos))
       exit(0)
